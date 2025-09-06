40721d31f28d6aa358cb5cbe5e086cac7d5e614e
MEHUL BANSAL <123830412+MEHULBANSAL2003@users.noreply.github.com>
2025-09-07 00:02:01 +0530

    Update Constants.jsx

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,4 +1,4 @@
 export const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:7777"
-    : "https://devtinder.crabdance.com/api";
+    : "https://devtinder.twilightparadox.com/api";
